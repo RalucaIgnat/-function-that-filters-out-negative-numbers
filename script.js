115c69884dfdc49e48b2cf45c094adaf9942f81d
Raluca Ignat <ralucaignat56@gmail.com>
2022-11-01 16:24:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var numberArray = [12, -26, 32, 14, 27, -31, -17, 0, -1, 10];
+
+var result = numberArray.filter(testFunction);
+
+function testFunction(values){
+
+return values < 0;
+
+}
+
+console.log("Array of Negative Numbers: " , result);
